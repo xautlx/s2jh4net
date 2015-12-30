176874d96e831c5bb3043a2d49703836e4335576
Li Xia <xautlx@hotmail.com>
2015-12-30 15:02:59 +0800

    添加context.full.path参数以便在集群环境下通过不同端口访问保证获取到正确的完整上下文

--- src/main/java/lab/s2jh/core/web/filter/WebAppContextInitFilter.java
+++ src/main/java/lab/s2jh/core/web/filter/WebAppContextInitFilter.java
@@ -13,8 +13,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import lab.s2jh.core.context.SpringContextHolder;
 import lab.s2jh.support.service.DynamicConfigService;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
@@ -44,12 +46,17 @@ public class WebAppContextInitFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse rep, FilterChain chain) throws IOException, ServletException {
         if (WEB_CONTEXT_FULL_URL == null) {
             HttpServletRequest request = (HttpServletRequest) req;
-            StringBuffer sb = new StringBuffer();
-            sb.append(request.getScheme()).append("://").append(request.getServerName());
-            sb.append(request.getServerPort() == 80 ? "" : ":" + request.getServerPort());
-            sb.append(request.getContextPath());
+            DynamicConfigService dynamicConfigService = SpringContextHolder.getBean(DynamicConfigService.class);
+            String contextPath = dynamicConfigService.getString("context.full.path");
+            if (StringUtils.isBlank(contextPath)) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(request.getScheme()).append("://").append(request.getServerName());
+                sb.append(request.getServerPort() == 80 ? "" : ":" + request.getServerPort());
+                sb.append(request.getContextPath());
+                contextPath = sb.toString();
+            }
             //当前应用的完整上下文路径，一般用于邮件、短信等需要组装完整访问路径之用
-            WEB_CONTEXT_FULL_URL = sb.toString();
+            WEB_CONTEXT_FULL_URL = contextPath;
             //设置当前WEB_ROOT根目录到配置属性以便在单纯的Service运行环境取到应用根目录获取WEB-INF下相关资源
             WEB_CONTEXT_REAL_PATH = request.getServletContext().getRealPath("/");
             logger.info("Init setup WebApp Context Info: ", WEB_CONTEXT_FULL_URL);
